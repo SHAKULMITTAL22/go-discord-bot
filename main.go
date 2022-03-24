45ea4fc43cb28d041dd4c7d9244b4acb6a52bfde
DarioZachow <dario.zachow@deutschebahn.com>
2022-03-24 11:00:23 +0100

    start bot in main file

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
- package main
- 
- import "fmt"
- 
- func main() {
-    fmt.Println("Hello, World!")
+package main
+
+import (
+	"github.com/helltf/go-discord-bot/bot"
+)
+
+func main() {
+	discord_secret := GetEnvVariable("DISCORD_SECRET")
+	bot.StartBot(discord_secret)
 }
