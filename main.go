d60dbe766f6032419cc6be99ad16ed106ae43cf6
DarioZachow <dario.zachow@deutschebahn.com>
2022-03-24 13:54:52 +0100

    starting bot now

--- main.go
+++ main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/helltf/go-discord-bot/bot"
+	"github.com/helltf/go-discord-bot/env"
+	
 )
 
+
 func main() {
-	LoadEnv()
-	discord_secret := GetEnvVariable("DISCORD_SECRET")
-	bot.StartBot(discord_secret)
+	success := env.LoadEnv()
+
+	if !success {
+		fmt.Println("Error while loading env file")
+	}
+
+	discordBot := bot.CreateBot()
+
+	discordBot.Start()
 }
