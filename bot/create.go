38f019fa2873fe5fbabf4acc64773943ca6f1019
DarioZachow <dario.zachow@deutschebahn.com>
2022-03-24 15:51:04 +0100

    added creation of discord bot via lib

--- bot/create.go
+++ bot/create.go
@@ -2,13 +2,20 @@ package bot
 
 import(
 	"github.com/helltf/go-discord-bot/env"
+	"github.com/bwmarrin/discordgo"
 )
 
 var (
 	botToken = "DISCORD_BOT_TOKEN"
 )
 
-func CreateBot() *Bot {
+func CreateBot() *discordgo.Session {
 	discordSecret := env.GetEnvVariable(botToken)
-	return &Bot{discordSecret, "!"}
+	bot, err := discordgo.New("Bot " + discordSecret )
+	
+	if(err != nil){
+		return nil
+	}
+
+	return bot
 }
