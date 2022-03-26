e41425d2ebffef28db5d21461656f379215bf078
DarioZachow <dario.zachow@deutschebahn.com>
2022-03-26 15:53:18 +0100

    added test file

---
+++ bot/bot_test.go
@@ -0,0 +1,26 @@
+package bot_test
+
+import (
+	"testing"
+
+	"github.com/helltf/go-discord-bot/bot"
+	"github.com/helltf/go-discord-bot/env"
+	"github.com/stretchr/testify/assert"
+) 
+
+
+func TestGetDiscordSecret(t *testing.T){
+	env.LoadEnv("test")
+
+	secret := bot.GetDiscordSecret()
+	assert.NotEqual(t, secret, "")
+}
+
+func TestCreateNewBot(t *testing.T){
+	env.LoadEnv("test")
+
+	secret := bot.GetDiscordSecret()
+	bot.CreateNewBot(secret)
+}
+
+
