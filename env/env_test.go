f27f3289a84d76a471ab6644068ea695953ca3a1
DarioZachow <dario.zachow@deutschebahn.com>
2022-03-26 15:54:15 +0100

    added test file for env

---
+++ env/env_test.go
@@ -0,0 +1,18 @@
+package env_test
+
+import(
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"github.com/helltf/go-discord-bot/env"
+)
+
+
+func TestLoadProdEnv(t *testing.T){
+	success := env.LoadEnv("prod")
+	assert.False(t, success)
+}
+
+func TestLoaTestEnv(t *testing.T){
+	success := env.LoadEnv("test")
+	assert.True(t, success)
+}
