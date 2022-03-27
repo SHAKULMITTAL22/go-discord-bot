b53478b8c33679bfe0cf14864d05758c87f2e261
helltf <Dario.Zachow5530@gmail.com>
2022-03-27 23:28:13 +0200

    updated func to send value

--- bot/handler.go
+++ bot/handler.go
@@ -2,7 +2,6 @@ package bot
 
 import (
 	"strings"
-
 	"github.com/bwmarrin/discordgo"
 )
 
@@ -18,3 +17,20 @@ func HasPrefix(message string) bool {
 func IsSameId(s *discordgo.Session, m *discordgo.MessageCreate) bool {
 	return m.Author.ID == s.State.User.ID
 }
+
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
+	result := make(chan string)
+
+	if HasPrefix(m.Content) || IsSameId(s, m){
+		 return
+	} 
+	
+	go RunCommand(m, result)
+	response := <- result
+	s.ChannelMessageSend(m.ChannelID, response)
+}
+
+func RunCommand(m *discordgo.MessageCreate, result chan string) {
+	result <- m.Content
+}
+
