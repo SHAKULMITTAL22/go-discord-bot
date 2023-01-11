c6ab7a31cee42683819f7ca8cf81ca3e9c655c1d
helltf <dario.zachow5530@gmail.com>
2023-01-11 20:31:14 +0100

    feat: update deps

--- internal/bot/bot.go
+++ internal/bot/bot.go
@@ -1,1 +1,37 @@
 package bot
+
+import (
+	"log"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func CreateNewBotConnection() {
+	discordSecret := ""
+	discordBot := CreateNewBot(discordSecret)
+	OpenConnection(discordBot)
+}
+
+func StartHandlers(bot *discordgo.Session) {
+	bot.AddHandler(messageHandler)
+}
+
+func OpenConnection(discordBot *discordgo.Session) {
+	errOpen := discordBot.Open()
+
+	if errOpen != nil {
+		panic(errOpen)
+	}
+}
+
+func CreateNewBot(discordSecret string) *discordgo.Session {
+	discordBot, err := discordgo.New("Bot " + discordSecret)
+
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("Succesfully created new Session")
+
+	return discordBot
+}
--- main.go
+++ main.go
@@ -1,21 +1,9 @@
 package main
 
-import (
-	"fmt"
-	"github.com/helltf/go-discord-bot/bot"
-	"github.com/helltf/go-discord-bot/env"
-)
-
+import "github.com/helltf/go-discord-bot/internal/bot"
 
 func main() {
-	success := env.LoadEnv("prod")
-
-	if !success {
-		fmt.Println("Error while loading env file")
-	}
-
 	bot.CreateNewBotConnection()
 	<-make(chan struct{})
 	return
 }
-
